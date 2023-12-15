0170e405b6a223029e2f295c7e13114c972892bd
Jaitej <cjaitej@gmail.com>
2023-12-15 20:59:13 +0530

    Typos

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -5,7 +5,7 @@ export default function Project() {
   return (
     <section id="project" className="flex flex-col justify-center px-44 py-10">
       <h1 className="text-3xl font-bold text-center text-white pb-10 tittle">
-        Project
+        Projects
       </h1>
       <div className="flex flex-wrap justify-center gap-3">
         {project_data.map((item) => (
